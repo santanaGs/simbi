7b0ac1f502dc574ee299810d6c56a9142d0c9389
gabriel <gabrielsantana2267@gmail.com>
2024-05-16 08:54:50 -0300

    :sparkles: feat: Add swagger documentation

--- simbi-back/app/Http/Controllers/Api/V1/ApiProjectController.php
+++ simbi-back/app/Http/Controllers/Api/V1/ApiProjectController.php
@@ -6,17 +6,56 @@ use App\Http\Controllers\Controller;
 use App\Models\Project;
 use Illuminate\Http\Request;
 
+/** 
+ *@OA\Info(
+ *   title="Simbi Api",
+ *   version="1.0.1"
+ *) 
+ */
+
 class ApiProjectController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * @OA\Get(
+     *     path="/api/projects",
+     *     summary="Listar apenas 20 projetos para contruçao do carrossel",
+     *     operationId="getProjects",
+     *     tags={"Projects"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="List only 20 projects",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(
+     *                 @OA\Property(property="nome", type="string"),
+     *                 @OA\Property(property="municipio", type="string"),
+     *                 @OA\Property(property="uf", type="string"),
+     *                 @OA\Property(property="resumo", type="string"),
+     *                 @OA\Property(property="valor_aprovado", type="string"),
+     *                 @OA\Property(property="valor_captado", type="string")
+     *             )
+     *         )
+     *     )
+     * )
      */
+
     public function index()
     {
-        return Project::take(20)->get();
+        return Project::take(20)->get(['nome', 'municipio', 'uf', 'resumo', 'valor_aprovado', 'valor_captado']);
     }
 
-
+    /**
+     * @OA\Get(
+     *     path="/api/projects/all",
+     *     summary="Listar todos os projetos com detalhes",
+     *     operationId="getAllProjects",
+     *     tags={"Projects"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Lista de projetos com detalhes",
+     *     )
+     * )
+     */
     public function getAll()
     {
         return Project::all();
--- simbi-back/routes/api.php
+++ simbi-back/routes/api.php
@@ -4,9 +4,31 @@ use App\Http\Controllers\Api\V1\ApiProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Rotas Públicas
+|--------------------------------------------------------------------------
+|
+| Aqui estão as rotas públicas acessíveis sem autenticação.
+|
+*/
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+/*
+|--------------------------------------------------------------------------
+| Rotas da API
+|--------------------------------------------------------------------------
+|
+| Aqui estão as rotas da API.
+|
+*/
+
+
 Route::get("/projects", [ApiProjectController::class, 'index']);
+
+
+
 Route::get("/projects/all", [ApiProjectController::class, 'getAll']);
